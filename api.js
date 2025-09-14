ace5963cc09c082c4d2f84bee28ea07553640016
Elliot Allen <elliotallendev@outlook.com>
2025-09-14 15:05:34 +0100

    Oh bootstrap I love you.
    Really need to remember .map when using arrays too

--- api.js
+++ api.js
@@ -1,10 +1,14 @@
-const url = " ";
+const API_KEY = "YR44JDSUL3UH2TWP";
 
-async function fetchData(address) {
-  const endpoint = url + address;
+
+export async function fetchStockData(symbol) {
+
+  // looks up stock based on stock symbol
+  const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`;
 
   try {
-    const response = await fetch(endpoint);
+    const response = await fetch(url);
+    
 
     if (!response.ok) {
       // Custom error messaging
@@ -13,8 +17,23 @@ async function fetchData(address) {
     }
 
     const data = await response.json();
-    console.log("JavaScript Object:", data);
-    return data; // Return data for use outside of function
+    // set top level key to const so that inner object can be accessed easier.
+    const stock = data["Global Quote"];
+
+    // update key value pairs into something more readable
+    const stockData = {
+    symbol: stock["01. symbol"],
+    open: stock["02. open"],
+    high: stock["03. high"],
+    low: stock["04. low"],
+    price: stock["05. price"],
+    previousClose: stock["08. previous close"],
+    change: stock["09. change"],
+    changePercent: stock["10. change percent"]
+  };
+
+    console.log("JavaScript Object:", stockData);
+    return stockData; // updated object returned for usage
 
   } catch (error) {
     // Handles Error
@@ -25,6 +44,66 @@ async function fetchData(address) {
       </div>`;
   }
 }
+
+export async function getChange(symbol) {
+  // references previous function to return solely change - will be used for top stock - kind of pointless but did it so I understand minimising repeated code
+  stock = await fetchStockData(symbol)
+  return stock.changepercent;
+}
+
+export async function fetchSearchResults(query) {
+
+  // retrieves a list of stocks based on symbol and/or name
+  const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${API_KEY}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  // alphavantage has it's own query engine that does best matches for you which can be stored in a JS array
+  if (!data.bestMatches) return [];
+
+    // data.bestMatches is a seriew of Key Values pairs
+    // maps the information  into an array of KV pairs with cleaner names - newKey: Value[oldKey]
+    const results = data.bestMatches.map(stock => ({
+      symbol: stock["1. symbol"],
+      name: stock["2. name"],
+      type: stock["3. type"],
+      region: stock["4. region"],
+      currency: stock["8. currency"]
+    }));
+    console.log(results)
+    return results;
+}
+
+export async function fetchTopFive () {
+
+  const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${API_KEY}`;
+
+  try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        // Custom error messaging
+        const errorMessage = response.statusText;
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json();
+      console.log("JavaScript Object:", data);
+      return data; // Return data for use outside of function
+
+    } catch (error) {
+      // Handles Error
+      console.error(error);
+      document.getElementByClass("quick-search").innerHTML = `
+        <div class="error-message">
+          <h3>Oops! ${error.message}</h3>
+        </div>`;
+    }
+  }
+
+
+  
 
 function handleClientErrors(response) {
   switch (response.status) {
--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-import { fetchData } from './api.js';
+import { fetchStockData, getChange, fetchTopFive, fetchSearchResults } from './api.js';
 
-fetchData("/Apple");
+
+document.addEventListener("DOMContentLoaded", () => {
+    // capure search button as const
+    const searchBtn = document.getElementById("openSearchModal");
+    // capture body of modal as const
+    const modalBody = document.getElementById("search-result-body");
+
+
+    searchBtn.addEventListener("click", async () => {
+    // data needs to be read from text input after button is clicked - data is empty on page load
+    const queryInput = document.getElementById("search-query");
+    // trim the value to prevent any uneeded issue
+    const query = queryInput.value.trim();
+    // calls function to get list of results and awaits response. Modal will open once data arrives
+    const results = await fetchSearchResults(query);
+
+    const html = `
+            <ul style="list-style: none; padding: 0; margin: 0;">
+                ${results.map(result => `
+                    <li style="padding: 10px; border-bottom: 1px solid #ddd;">
+                    <strong>${result.symbol}</strong> - ${result.name}<br>
+                    Type: ${result.type}, Region: ${result.region}, Currency: ${result.currency}
+                    </li>
+                `).join('')}
+            </ul>
+        `;
+
+    //updates modal via const rather than explicit html references                
+    modalBody.innerHTML = html;
+    
+    });
+});
